3d6364fcaaf3f54ca3dc09f5de41e3d5ec2252fc
ChunTaeKyung <ctk0327@naver.com>
2019-02-14 00:51:22 +0900

    apply gitignore

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ rtm.start();
 const web = new WebClient(token);
 
 let list = [];
+let need;
 let users = ['ctk0327', 'oyrin9595', 'ooiidd', 'klee', 'dragon4499', 'drcobi', 'fluxus', 'morot2'];
 // let users = ['klee'];
 let scores = [0, 0, 0, 0, 0, 0, 0, 0];
@@ -33,13 +34,14 @@ let solved = {
 web.channels.list()
     .then((res) => {
         // Take any channel for which the bot is a member
-        const channel = res.channels.find(c => c.is_member && c.name === 'general');
-
+        const channel = res.channels.find(c => c.is_member && (c.name === 'test' || c.name === 'general'));
+        channel.id="CFBAE8EQ2"; //general 채널 ID
+        //channel.id="CFYP0Q3NV" //test 채널 ID
         if (channel) {
             rtm.on('message', (message) => {
 
-                if(message.text==="안내"){
-                    let info="안녕하세요 알고파 봇입니다.\n" +
+                if (message.text === "안내") {
+                    let info = "안녕하세요 알고파 봇입니다.\n" +
                         "이번주 문제를 입력하고 십으편 : 이번주 문제 (느낌표로 구분된 문제)\n" +
                         "현재 점수를 알고 싶으면  : 현재 점수\n" +
                         "이번주 풀이 현황을 알고 싶으면 : 이번주 풀이\n" +
@@ -53,7 +55,7 @@ web.channels.list()
                         .catch(console.error);
                 }
 
-                if (message.text.slice(0, 5) === "점수 정산") {
+                if (message.text.slice(0, 6) === "@점수 정산") {
                     users.forEach(a => {
                         getThisWeekSolved(a).then(function (tried) {
                             getTotalSolved(tried, a).then(function (resolve) {
@@ -69,7 +71,7 @@ web.channels.list()
                     });
                 }
 
-                if (message.text.slice(0, 5) === "현재 점수") {
+                if (message.text.slice(0, 6) === "@현재 점수") {
                     users.forEach(a => {
                         rtm.sendMessage(a + "의 점수는 " + scores[users.indexOf(a)] + " 입니다.", channel.id)
                             .then((res) => {
@@ -79,7 +81,7 @@ web.channels.list()
                     });
                 }
 
-                if (message.text === "점수 초기화") {
+                if (message.text === "@점수 초기화") {
                     scores = [0, 0, 0, 0, 0, 0, 0, 0];
                     solved = {
                         'ctk0327': [],
@@ -98,7 +100,7 @@ web.channels.list()
                         .catch(console.error);
                 }
 
-                if (message.text.slice(0, 6) === "이번주 문제") {
+                if (message.text.slice(0, 7) === "@이번주 문제") {
                     const prob = message.text.slice(8);
                     makeProblem(prob).then(function () {
                         rtm.sendMessage("이번주 해당 문제는 " + list.toString() + " 입니다.", channel.id)
@@ -109,7 +111,49 @@ web.channels.list()
                     });
 
                 }
-                if (message.text === "이번주 풀이") {
+
+                if (message.text.slice(0, 6) === "@필수 문제") {
+                    const prob = message.text.slice(7);
+                    makeNeedProblem(prob).then(function () {
+                        rtm.sendMessage("이번주 필수 문제는 " + need.toString() + " 입니다.", channel.id)
+                            .then((res) => {
+                                console.log('Message sent: ', res.ts);
+                            })
+                            .catch(console.error);
+                    });
+
+                }
+
+                if (message.text.slice(0, 3) === "@벌금") {
+                    users.forEach(a => {
+                        let cntFee = 0;
+                        let cntTotalSolved=0;
+                        let cntNeedSolved=0;
+                        if (solved[a].indexOf(need) === -1) {
+                            cntFee++;
+                        }else{
+                            cntNeedSolved++;
+                        }
+                        const thisWeekSolved=solved[a];
+                        thisWeekSolved.forEach(a=>{
+                            if(list.indexOf(a)!==-1){
+                                cntTotalSolved++;
+                            }
+                        });
+                        if(cntTotalSolved-cntNeedSolved===0){
+                            cntFee++;
+                        }
+
+                        rtm.sendMessage(a + "의 이번주 벌금은 " + cntFee * 3000 + "입니다.", channel.id)
+                            .then((res) => {
+                                console.log('Message sent: ', res.ts);
+                            })
+                            .catch(console.error);
+                    });
+                }
+
+
+                if (message.text === "@이번주 풀이") {
                     users.forEach(a => {
                         getThisWeekSolved(a).then(function (tried) {
                             let message;
@@ -158,6 +202,19 @@ const makeProblem = function (prob) {
     });
 };
 
+const makeNeedProblem = function (prob) {
+    return new Promise(function (resolve, reject) {
+        try {
+            prob.split(",").forEach(a => {
+                need = a;
+            });
+            resolve(need);
+        } catch (e) {
+            reject(need);
+        }
+    });
+};
+
 const getThisWeekSolved = function (user) {
     return new Promise(function (resolve, reject) {
         const url = 'https://www.acmicpc.net/user/';
@@ -168,7 +225,7 @@ const getThisWeekSolved = function (user) {
                 const data = $(this);
                 console.log(data.text().toString());
                 list.forEach((a) => {
-                    if (Number(a)=== Number(data.text())) {
+                    if (Number(a) === Number(data.text())) {
                         tried.push(a);
                     }
                 });
